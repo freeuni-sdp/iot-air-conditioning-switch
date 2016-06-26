01bae2415fbd0d7799991a340197ec059115d42d
piopao <ekikn12@freeuni.edu.ge>
2016-06-26 22:40:04 +0400

    Added Post/Get @Giglema

---
+++ src/main/java/ge/edu/freeuni/sdp/iot/switch_air_conditioning/service/JsonWrapper.java
@@ -0,0 +1,23 @@
+package ge.edu.freeuni.sdp.iot.switch_air_conditioning.service;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+public class JsonWrapper {
+
+    private String status;
+
+    public JsonWrapper(){}
+    public JsonWrapper(String status){
+        setStatus(status);
+    }
+
+    public String getStatus(){
+        return this.status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+}
---
+++ src/main/java/ge/edu/freeuni/sdp/iot/switch_air_conditioning/service/Service.java
@@ -0,0 +1,31 @@
+package ge.edu.freeuni.sdp.iot.switch_air_conditioning.service;
+import ge.edu.freeuni.sdp.iot.switch_air_conditioning.model.CloudStorage;
+import ge.edu.freeuni.sdp.iot.switch_air_conditioning.model.SwitchEntity;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+public class Service {
+
+    @POST
+    @Path("/houses/{house_id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response insertSwitch(@PathParam("id") String id, JsonWrapper w){
+        SwitchEntity sw =  new SwitchEntity(id);
+        sw.setStatus(w.getStatus());
+        CloudStorage cloud = new CloudStorage();
+        cloud.insertOrReplaceSwitch(sw);
+        return Response.ok().build();
+    }
+
+    @GET
+    @Path("/houses/{house_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonWrapper retrieveSwitch(@PathParam("id") String id){
+        CloudStorage cloud = new CloudStorage();
+        SwitchEntity sw = cloud.retrieveSwitch(id);
+        JsonWrapper wr = new JsonWrapper(sw.getStatus());
+        return wr;
+    }
+
+}
